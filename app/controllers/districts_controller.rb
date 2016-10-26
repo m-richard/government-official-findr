adf4133929e43e0ef0cb0e09d05ba1aa7a3365bd
Marcus Richard <msrichard18@gmail.com>
2016-10-26 17:22:23 -0400

    added reps and sens to districts

--- app/controllers/districts_controller.rb
+++ app/controllers/districts_controller.rb
@@ -14,13 +14,23 @@ class DistrictsController < ApplicationController
 
   def create
     @district = District.new(district_params)
+    zip_code = @district.zip_code
+    api_data = Net::HTTP.get_response(URI("http://whoismyrepresentative.com/getall_mems.php?zip=#{zip_code}&output=json"))
+    result = JSON.parse(api_data.body)
+    @district.rep = result['results'][0]
+    @district.sen1 = result['results'][1]
+    @district.sen2 = result['results'][2]
     if @district.save
-      flash[:notice] = "District added successfully"
       redirect_to districts_path(@district)
     else
       flash[:notice] = @district.errors.full_messages.join(", ")
       render :new
     end
+  end
+
+  def destroy
+    District.destroy(params[:id])
+    redirect_to new_district_path
   end
 
   private
--- app/models/district.rb
+++ app/models/district.rb
@@ -1,5 +1,7 @@
 class District < ActiveRecord::Base
+  serialize :result, JSON
   has_many :addresses
+  attr_accessor :rep, :sen1, :sen2
 
   validates :zip_code, presence: true, length: { is: 5 }, numericality: true
 end
