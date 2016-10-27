b0ef3b6eaaed566dd2765c55d0c6edb4ba8ad511
Marcus Richard <msrichard18@gmail.com>
2016-10-27 14:57:14 -0400

    fixed show and create actions

--- app/controllers/districts_controller.rb
+++ app/controllers/districts_controller.rb
@@ -9,22 +9,37 @@ class DistrictsController < ApplicationController
 
   def show
     @district = District.find(params[:id])
-    @addresses = @district.addresses
+    zip_code = @district.zip_code
+    api_data = Net::HTTP.get_response(URI("http://whoismyrepresentative.com/getall_mems.php?zip=#{zip_code}&output=json"))
+    result = JSON.parse(api_data.body)
+    @district.rep_name = result['results'][0]['name']
+    @district.rep_district = result['results'][0]['district']
+    @district.rep_phone = result['results'][0]['phone']
+    @district.rep_link = result['results'][0]['link']
+    @district.rep_state = result['results'][0]['state']
+    @district.sen1_name = result['results'][1]['name']
+    @district.sen1_seat = result['results'][1]['district']
+    @district.sen1_phone = result['results'][1]['phone']
+    @district.sen1_link = result['results'][1]['link']
+    @district.sen2_name = result['results'][2]['name']
+    @district.sen2_seat = result['results'][2]['district']
+    @district.sen2_phone = result['results'][2]['phone']
+    @district.sen2_link = result['results'][2]['link']
   end
 
   def create
+    zip_code = params[:district][:zip_code]
     @district = District.new(district_params)
-    zip_code = @district.zip_code
     api_data = Net::HTTP.get_response(URI("http://whoismyrepresentative.com/getall_mems.php?zip=#{zip_code}&output=json"))
     result = JSON.parse(api_data.body)
     @district.rep = result['results'][0]
     @district.sen1 = result['results'][1]
     @district.sen2 = result['results'][2]
     if @district.save
-      redirect_to districts_path(@district)
+      redirect_to district_path(@district)
     else
       flash[:notice] = @district.errors.full_messages.join(", ")
-      render :new
+      render :create
     end
   end
 
