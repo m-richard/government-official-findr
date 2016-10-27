cda6d9178adbfa7ab1cc06faa06f42248f43f223
Marcus Richard <msrichard18@gmail.com>
2016-10-27 14:58:11 -0400

    attr_accessor

--- app/models/district.rb
+++ app/models/district.rb
@@ -3,14 +3,4 @@ class District < ActiveRecord::Base
   attr_accessor :rep, :sen1, :sen2
 
   validates :zip_code, presence: true, length: { is: 5 }, numericality: true
-
-  def self.save_data_from_api
-    response = Net::HTTP.get_response(URI("http://whoismyrepresentative.com/getall_mems.php?zip=#{zip_code}&output=json"))
-    result = JSON.parse(response)
-    data = result.map do |line|
-      d = District.new
-      d.save
-    end
-    data.select(&:persisted?)
-  end
 end
