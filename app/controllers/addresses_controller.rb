24184d150f9833d5e74bda9c85e4043ee08aa36b
Marcus Richard <msrichard18@gmail.com>
2016-10-28 13:17:24 -0400

    deploy

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 class AddressesController < ApplicationController
   require 'net/http'
   def index
--- app/controllers/api/v2/senators_controller.rb
+++
@@ -1,9 +0,0 @@
-class Api::DistrictsController < ApiController
-  def create
-    @district = District.find(params[:district_id])
-    render json: { districts: districts }, status: :ok
-  end
-end
-
-
-# https://www.googleapis.com/civicinfo/v2/representatives?address=53+Bothfeld+Rd%2C+Newton%2C+MA+02459&includeOffices=true&levels=subLocality1&levels=subLocality2&levels=special&levels=regional&levels=locality&roles=legislatorUpperBody&roles=legislatorLowerBody&fields=normalizedInput%2Coffices%2Cofficials&key={YOUR_API_KEY}
